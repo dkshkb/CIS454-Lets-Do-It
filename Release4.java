8bcf0defa178cd187f93d90da7e01523b6b3eec7
dkshkb <48193062+dkshkb@users.noreply.github.com>
2021-04-01 11:14:17 -0400

    implement scanner parser, upload syllabus to generate task list

--- Release4.java
+++ Release4.java
@@ -6,6 +6,18 @@ import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.lang.Object;
+import java.util.*;
+import java.time.*;
+import java.time.format.*;
+import java.text.SimpleDateFormat;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.io.File;
+import java.io.*;
+import java.util.Map;
 
 public class Release4 {
     private static JFrame frame = new JFrame();
@@ -14,6 +26,11 @@ public class Release4 {
     private static String[] to_do_string = {"Task1", "Task2", "Task3"};
     private static JList to_do_list;
     private static String suggestion;
+
+    //creates hashmap to store all the assignments in and a count to denote the different sllyabi that have been parsed
+    public static int sllyCount = 0;
+    static HashMap assignments = new HashMap<String , String[]>();
+    static int numAssignments;
 
 
     public static void landing_page () {
@@ -97,7 +114,13 @@ public class Release4 {
         c.gridy = 2;
         mainPanel.add(suggestionButton, c);
 
-        JButton uploadButton = new JButton("Upload");
+        JButton uploadButton = new JButton(new AbstractAction("Upload") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.remove(mainPanel);
+                upload_page();
+            }
+        });
         c.gridx = 1;
         c.gridy = 3;
         mainPanel.add(uploadButton, c);
@@ -284,6 +307,189 @@ public class Release4 {
         });
         exitButton.setBounds(210, 170, 120, 25);
         mainPanel.add(exitButton);
+
+        frame.setVisible(true);
+    }
+
+    public static void getLocation(String loc) throws FileNotFoundException{
+        //function to get the string
+        //take the location from the user entry and then parse the file specified at the location
+        try
+        {
+            //do all this manually because Im lazy
+            //create a new file with the location entered by the user
+            File fileLoc = new File(loc);
+            Scanner scan = new Scanner(fileLoc);
+            String className = scan.nextLine();
+            //get the number of assignments from the top of the sllyabus
+            numAssignments = Integer.parseInt(scan.nextLine());
+            String assignmentName;
+            String assignmentDesc;
+            String assignmentDueDate;
+
+            //the path of my C:\Users\trelo\OneDrive\Desktop\sllyabusExample.txt
+            // for loop to parse the sllyabus
+            for (int i = 0; i < numAssignments; i++){
+                //name of the key for the hashmap
+                String dicName = "hw" + (sllyCount + i);
+                sllyCount++;
+                //Name of the assignment stored in the
+                assignmentName = scan.nextLine();
+                //Description of the assignment
+                assignmentDesc = scan.nextLine();
+                //The due date of the assignment
+                assignmentDueDate = scan.nextLine();
+                //Put the aspects of the assignment into an array of strings
+                String[] hwMap = {assignmentName,assignmentDesc,assignmentDueDate};
+                //Place the name, desc and due date of the assignment into the assignment hashmap entry respectively
+                assignments.put(dicName, hwMap);
+            }
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+    }
+
+    public static void upload_page(){
+
+        mainPanel = new JPanel();
+        //frame = new JFrame();
+        frame.setSize(700, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(mainPanel);
+        mainPanel.setLayout(null);
+
+        JLabel letsDoIt = new JLabel("Enter file path of sllyabus to get assignemnt");
+        letsDoIt.setBounds(100, 20, 300, 25);
+        mainPanel.add(letsDoIt);
+
+        JLabel pathLabel = new JLabel("Path");
+        pathLabel.setBounds(30, 50, 80, 25);
+        mainPanel.add(pathLabel);
+
+        JTextField userText = new JTextField();
+        userText.setBounds(120, 50, 300, 25);
+        mainPanel.add(userText);
+
+        JButton uploadButton = new JButton(new AbstractAction("Upload") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                assignments.clear();
+                String path = userText.getText();
+                try {
+                    getLocation(path);
+                } catch (FileNotFoundException fileNotFoundException) {
+                    fileNotFoundException.printStackTrace();
+                }
+                frame.remove(mainPanel);
+                show_syllabus_page();
+            }
+
+        });
+        uploadButton.setBounds(30, 110, 80, 25);
+        mainPanel.add(uploadButton);
+
+        JButton exit = new JButton(new AbstractAction("Exit") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                frame.remove(mainPanel);
+                landing_page();
+            }
+        });
+        exit.setBounds(120, 110, 80, 25);
+        mainPanel.add(exit);
+
+
+
+        frame.setVisible(true);
+    }
+
+    public static void show_syllabus_page(){
+
+        mainPanel = new JPanel();
+        //frame = new JFrame();
+        frame.setSize(700, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(mainPanel);
+        mainPanel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+
+        JLabel letsDoIt = new JLabel("List of assignments scanned from the syllabus is shown below");
+        letsDoIt.setBounds(60, 20, 400, 25);
+        mainPanel.add(letsDoIt);
+
+        c.weighty = 0.5;
+        c.gridwidth = 1;
+        c.gridx = 0;
+        c.gridy = 0;
+        mainPanel.add(letsDoIt, c);
+
+        JScrollPane taskScrollPanel;
+
+        //String[] assignment_string = {};
+        ArrayList<String> lst = new ArrayList<String>();
+        //assignments.forEach((Key, Value) ->
+        // System.out.println( "Name: " + ((String[])(Value))[0]
+        // + " \n Desc: " + ((String[])(Value))[1]
+        // + " \n Due Date: " + ((String[])(Value))[2]));
+        Iterator<Map.Entry<String, String[]>> it = assignments.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<String, String[]> set = (Map.Entry<String, String[]>) it.next();
+            String str = "Name: " + set.getValue()[0]
+                    + " (" + set.getValue()[1] + ") Due: "
+                    + set.getValue()[2];
+            lst.add(str); // Need to modify
+        }
+        String[] assignment_string = lst.toArray(new String[0]);
+
+        JList assignment_list = new JList(assignment_string);
+        taskScrollPanel = new JScrollPane(assignment_list);
+        taskScrollPanel.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        taskScrollPanel.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+
+        c.gridwidth = 1;
+        c.gridheight = 1;
+        c.weighty = 0;
+        c.gridx = 0;
+        c.gridy = 1;
+        c.ipady = 100;
+        c.ipadx = 100;
+        mainPanel.add(taskScrollPanel, c);
+
+        JButton addButton = new JButton(new AbstractAction("Add to task-list") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //append assignment_string to to_do_string
+
+                ArrayList<String> lst = new ArrayList<String>(Arrays.asList(to_do_string));
+                for (int i = 0; i < assignment_string.length; i++) {
+                    lst.add(assignment_string[i]);
+                }
+                to_do_string = lst.toArray(new String[0]);
+                frame.remove(mainPanel);
+                landing_page();
+            }
+        });
+        c.ipady = 50;
+        //c.weighty= 0;
+        c.gridheight = 1;
+        c.gridx = 0;
+        c.gridy = 2;
+        mainPanel.add(addButton, c);
+
+        JButton backButton = new JButton(new AbstractAction("Back") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.remove(mainPanel);
+                upload_page();
+            }
+        });
+        c.gridx = 0;
+        c.gridy = 3;
+        mainPanel.add(backButton, c);
+
 
         frame.setVisible(true);
     }
